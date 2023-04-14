0cc800111acc7acc263565fa263febcf9e6bec2e
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-04-14 18:05:51 -0500

    project save

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import tax from "./tax-rates.js";
-
+import Sidebar from "./Sidebar.jsx";
+import Content from "./content.jsx";
 
 function App() {
 
   return (
     <div id={"app"}>
-
+        <Sidebar></Sidebar>
+        <Content></Content>
     </div>
   )
 }
---
+++ src/content.jsx
@@ -0,0 +1,18 @@
+function Content() {
+
+
+    return (
+        <div className="content">
+            <header className="header">
+                Net Income:$0   Tax: $0   Tax rate: 0%
+            </header>
+            <main className="main">
+                <div className="tax-calculation">
+
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Content
