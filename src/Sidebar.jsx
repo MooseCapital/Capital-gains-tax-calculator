684d62f1356c1595ca35607a4d981b2c77ca8304
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-04-14 23:51:18 -0500

    project save

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,9 +1,40 @@
+import {useState} from "react";
+
 function Sidebar() {
 
+    const [formData, setFormData] = useState({
+        capitalGains: "", stateTaxRate: "0"
+    })
+
+    function handleChange(event) {
+        console.log("test")
+        setFormData(prevForm => {
+                return {
+                    ...prevForm,
+                    [event.target.name] : event.target.value,
+
+                }
+            })
+    }
+
+    function submitForm(event) {
+        event.preventDefault()
+        console.log(formData)
+    }
 
     return (
         <div className="sidebar">
+        <div className="show-tax-box">
 
+        </div>
+            <form action="" onSubmit={submitForm}>
+
+                <label htmlFor="capitalGains">Capital Gains</label>
+                <input onChange={handleChange} id={"capitalGains"} name={"capitalGains"} value={formData.grossIncome} type="text"/>
+                <label htmlFor="stateTaxRate">State tax rate</label>
+                <input onChange={handleChange} id={"stateTaxRate"} name={"stateTaxRate"} value={formData.stateTaxRate} type="text"/>
+                <button>Calculate</button>
+            </form>
         </div>
     )
 }
--- src/content.jsx
+++ src/content.jsx
@@ -1,14 +1,32 @@
-function Content() {
+import {useState} from "react";
 
+function Content() {
+    const [image, setImage] = useState("public/close.svg")
+
+    function deleteTax() {
+
+    }
 
     return (
         <div className="content">
             <header className="header">
-                Net Income:$0   Tax: $0   Tax rate: 0%
+                <p>Capital gains</p>
+                <p>Tax rate</p>
+                <p>Tax paid</p>
+                <p>Net gain</p>
             </header>
             <main className="main">
                 <div className="tax-calculation">
-
+                    <div className={"tax-1"}>$20,000</div>
+                    <div className={"tax-2"}>%15</div>
+                    <div className={"tax-3"}>$4,000</div>
+                    <div className={"tax-4"}>$16,000</div>
+                    <img
+                        onMouseEnter={() => setImage(prevImg => "public/close_prelight.svg")}
+                        onMouseLeave={() => setImage(prevState =>   "public/close.svg" )}
+                        onClick={deleteTax}
+                        className={"close"} src={image} alt=""
+                    />
                 </div>
             </main>
         </div>
