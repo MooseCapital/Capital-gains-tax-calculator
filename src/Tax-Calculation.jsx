6e210cfc0387d44fe4a388b99bef372d02a28f81
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-04-16 00:17:28 -0500

    images and font imported from public folder

--- src/Tax-Calculation.jsx
+++ src/Tax-Calculation.jsx
@@ -1,8 +1,10 @@
 import {useState} from "react";
 import tax from "./tax-rates.js";
+const closeImg = new URL('/public/close.svg', import.meta.url).href
+const closePrelightImg = new URL('/public/close_prelight.svg', import.meta.url).href
 
 function Tax_calculation(props) {
-    const [image, setImage] = useState("public/close.svg")
+    const [image, setImage] = useState(closeImg)
 
 
 
@@ -15,8 +17,8 @@ function Tax_calculation(props) {
                     <div className={"tax-3"}>{props.taxPaid}</div>
                     <div className={"tax-4"}>{props.NetGains}</div>
                     <img
-                        onMouseEnter={() => setImage(prevImg => "public/close_prelight.svg")}
-                        onMouseLeave={() => setImage(prevState =>   "public/close.svg" )}
+                        onMouseEnter={() => setImage(prevImg => closePrelightImg)}
+                        onMouseLeave={() => setImage(prevState =>   closeImg )}
                         onClick={props.deleteTaxDiv}
                         className={"close"} src={image} alt=""
                     />
